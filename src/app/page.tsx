dc5266fd8b54879311fdd5e5b650a0f8c649e2f7
kotsios34 <papouikonstantinos8@gmail.com>
2024-10-30 13:18:06 +0200

    second change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>My first ci/cd action and deployment.</li>
+          <li>My first ci/cd action and deployment.kostis</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
