88552064554c5c622fdc08758ddba8f52c12027b
kotsios34 <papouikonstantinos8@gmail.com>
2024-11-04 08:51:42 +0200

    home page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,34 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-yellow/[.05] dark:bg-amber-500/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>My first ci/cd action and deployment.kostis</li>
-        </ol>
+  <div className=' h-full w-full'>
+    <div className=" min-h-screen bg-[#5893D4] w-full text-white min-w-[344px] ">
+        {/**<Navbar/>**/}
+        <div className="h-fit w-full bg-[#5893D4] absolute top-20  flex items-center justify-center flex-col min-w-[344px] ">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div className="xxxll:w-[70%] xll:w-[80%] 600:w-[72%] 535:w-[80%] 426:w-[85%] base:w-[92%] p-6">
+                   <h2 className="scroll-m-20  text-3xl font-semibold tracking-tight first:mt-0">
+                     My trips
+                   </h2>
+            </div>
+
+            <div className=" xxxll:w-[70%] xll:w-[80%] 600:w-[72%] 535:w-[80%] 426:w-[85%] base:w-[92%]   flex justify-between  ">
+          { /**   <Dropdown/>
+             <Createtripmodal/>  */ }
+            </div>
+
+            <div className=" xxxll:w-[70%] xll:w-[80%] 600:w-[72%] 535:w-[80%] 426:w-[85%] base:w-[92%]   p-3  ">
+                <div className=" p-3 flex gap-3 flex-wrap ">
+                 {/**    {getalltrip.map((trip , key) =>
+                        <Tripmodal key={key} tripTitle={trip.tripName} tripId={trip.id as string}/>  
+                 ) }            */  }         
+                </div>
+
+
+            </div>
+
+        </div>  
     </div>
-  );
+  </div>
+  )
 }
