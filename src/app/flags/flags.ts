bff3aeb6a8ff566f7b2fc8d27743c04c4c236ea7
kotsios34 <papouikonstantinos8@gmail.com>
2024-11-04 19:29:00 +0200

    soe

--- src/app/flags/flags.ts
+++ src/app/flags/flags.ts
@@ -3,7 +3,7 @@ import { unstable_flag as flag  } from "@vercel/flags/next"
 export const allowExpensiveAI = flag({
     key: "expensive-ai" , 
     description : "Use the expensive AI model" ,
-    decide : async () => false , 
+    decide : async () => true , 
     defaultValue : true
 })
 
