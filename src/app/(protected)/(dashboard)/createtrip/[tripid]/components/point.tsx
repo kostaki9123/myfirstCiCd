19826c34321a63d7c5045094989446dee8fdcbe7
kotsios34 <papouikonstantinos8@gmail.com>
2025-09-26 20:43:48 +0300

    fix propblem with responsive create moving box

--- src/app/(protected)/(dashboard)/createtrip/[tripid]/components/point.tsx
+++ src/app/(protected)/(dashboard)/createtrip/[tripid]/components/point.tsx
@@ -188,7 +188,7 @@ const Point =  (props:Props) => {
         </div>
 
 
-         <DialogContent className=" h-[400px] 820:h-fit 820:w-fit w-[90%] 450:w-[350px]  absolute ">
+         <DialogContent className=" ml-2 450:ml-0 min-w-[280px] h-[400px] 820:h-fit 820:w-fit w-[90%] 450:w-[350px]  absolute ">
             <DialogHeader>
               <DialogTitle className=' text-xl'>
                    <EditableText
