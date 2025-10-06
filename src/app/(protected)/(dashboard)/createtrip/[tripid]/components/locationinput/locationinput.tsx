7632e9e6b282d3ff1bef4ad82546e0c83f41c9ad
kotsios34 <papouikonstantinos8@gmail.com>
2025-10-06 21:53:32 +0300

    add reccomended places inputh

--- src/app/(protected)/(dashboard)/createtrip/[tripid]/components/locationinput/locationinput.tsx
+++ src/app/(protected)/(dashboard)/createtrip/[tripid]/components/locationinput/locationinput.tsx
@@ -274,7 +274,7 @@ function PlaceSearch({ onPlaceSelected }: PlaceSearchProps) {
             {/* ✅ Adjust min-height when keyboard is open */}
             <div
               className={`flex-1 overflow-y-auto border-2 border-red-600 transition-all duration-200 ${
-                keyboardOpen ? "max-h-[300px]" : ""
+                keyboardOpen ? "max-h-[250px]" : ""
               }`}
             >
               {predictions.length > 0 ? (
