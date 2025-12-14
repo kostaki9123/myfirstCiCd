a586e570909f404bbc9e7091a3d029d728389c3c
kotsios34 <papouikonstantinos8@gmail.com>
2025-12-14 21:26:44 +0200

    add connection with affialiate links in itinerary

--- src/app/api/links/[placeId]/route.ts
+++ src/app/api/links/[placeId]/route.ts
@@ -17,3 +17,34 @@ export async function GET(
 
   return NextResponse.json(link);
 }
+
+export async function PUT(
+  req: Request,
+  { params }: { params: any }
+) {
+  const placeId = params.placeId;
+  const { affiliate_url, source } = await req.json();
+
+  const link = await prisma.placeAffiliateLink.update({
+    where: { place_id: placeId },
+    data: {
+      affiliate_url,
+      source,
+    },
+  });
+
+  return NextResponse.json(link);
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: any }
+) {
+  const placeId = params.placeId;
+
+  await prisma.placeAffiliateLink.delete({
+    where: { place_id: placeId },
+  });
+
+  return NextResponse.json({ success: true });
+}
--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,47 +1,14 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
 import { redirect } from "next/navigation";
-import { currentUser } from "@clerk/nextjs/server"; 
+import { currentUser } from "@clerk/nextjs/server";
 
-
-export async function GET(req: Request) {
+export async function GET() {
   const user = await currentUser();
   if (!user) redirect("/sign-in");
-  console.log('my api get links run')
+
   const links = await prisma.placeAffiliateLink.findMany();
-  console.log('all links', links)
   return NextResponse.json(links);
-}
-
-export async function PUT(
-  req: Request,
-  { params }: { params: { placeId: string } }
-) {
-  const { affiliate_url, source } = await req.json();
-  const { placeId } = params;
-
-  const link = await prisma.placeAffiliateLink.update({
-    where: { place_id: placeId },
-    data: {
-      affiliate_url,
-      source,
-    },
-  });
-
-  return NextResponse.json(link);
-}
-
-export async function DELETE(
-  req: Request,
-  { params }: { params: { placeId: string } }
-) {
-  const { placeId } = params;
-
-  await prisma.placeAffiliateLink.delete({
-    where: { place_id: placeId },
-  });
-
-  return NextResponse.json({ success: true });
 }
 
 export async function POST(req: Request) {
@@ -57,4 +24,3 @@ export async function POST(req: Request) {
 
   return NextResponse.json(link);
 }
-
