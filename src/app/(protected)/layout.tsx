b89c3f885adbef0c76a3d20fe3f3561192732624
KONSTANTINOS <konstantinos@10.98.148.165>
2026-03-13 15:48:45 +0200

    add loader for sign in and sign up pages

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,7 +2,6 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 import { signIn } from "./action";
-import ClientProtected from "./compoents/ClientProtected";
 
 export default async function ProtectedLayout({ children }: { children: ReactNode }) {
   const { userId, sessionId } = await auth();
