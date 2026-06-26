e24b2f4b8d27e91130b39af0e950553e93806ce9
KONSTANTINOS <konstantinos@MacBook-Air-KONSTANTINOS.local>
2026-06-26 10:35:03 +0300

    updata add place

--- src/app/(marketing)/component/OpenInBrowserBanner.jsx
+++ src/app/(marketing)/component/OpenInBrowserBanner.jsx
@@ -7,7 +7,7 @@ import { GoArrowRight } from "react-icons/go";
 
 
 const OpenInBrowserBanner = () => {
-  const [isInAppBrowser, setIsInAppBrowser] = useState(false)
+  const [isInAppBrowser, setIsInAppBrowser] = useState(true)
 
   useEffect(() => {
     const ua = navigator.userAgent || ''
@@ -23,33 +23,44 @@ const OpenInBrowserBanner = () => {
   if (!isInAppBrowser) return null
 
   return (
-   <div className="fixed top-0 left-0 right-0 z-[999] bg-[#010038]  text-white text-sm px-5 py-4 flex items-center justify-between shadow-lg">
+   <div className="fixed top-0 left-0 right-0 z-[999] border-white/10 bg-gradient-to-r from-[#0B1F6A] via-[#1747A6] to-[#0B1F6A] backdrop-blur-xl text-white text-sm px-5 py-4 flex items-center justify-between shadow-lg">
   {/* Logo on the left */}
-  <div className="flex items-center gap-2 shrink-0 ">
-              <Link
-                   href="/"
-                   className="relative w-[150px] md:ml-5 flex justify-center items-center "
-                 >
-                   <Image
-                     src="/ChatGPT Image 10 Μαρ 2026, 04_35_50 μμ.png"
-                     alt="Trip Planner Logo"
-                     width={90}
-                     height={90}
-                     className="object-contain mt-2 absolute left-0 md:left-3"
-                   />
-                 
-                   <h3 className="mt-1 text-xl font-semibold tracking-tight text-white absolute left-16 md:left-20">
-                     Tripaki
-                   </h3>
-                </Link>
-  </div>
+ {/* Logo */}
+<div className="flex items-center gap-2 shrink-0">
+  <Link
+    href="/"
+    className="relative w-[180px] md:ml-5 flex items-center "
+  >
+    <Image
+      src="/ChatGPT Image 10 Μαρ 2026, 04_35_50 μμ.png"
+      alt="Trip Planner Logo"
+      width={90}
+      height={90}
+      className="object-contain mt-2 absolute left-0 md:left-3"
+    />
+
+    <div className="absolute left-16 md:left-20">
+      <h3 className="text-xl font-semibold tracking-tight text-white">
+        Tripaki
+      </h3>
+
+     
+    </div>
+     <p className="text-[11px] absolute bottom-[-30px] ml-10 text-white/70 ">
+        Best experience in your browser
+    </p>
+  </Link>
+</div>  
 
   {/* Message on the right */}
-        <div className="  flex items-center justify-end gap-2  w-[300px]  py-3">
+        <div className="  flex items-center  my-auto justify-end gap-2  w-[300px]  py-3 mt-3">
         <GoArrowRight className=' text-2xl' />
         
             <p className="text-[11px] leading-tight text-center  font-medium">
-            Tap <strong>···</strong> then <strong>"Open in browser"</strong>
+            Tap  <span className="rounded-md px-2 py-1 font-bold">
+                  •••
+                </span>
+ then <strong>"Open in browser"</strong>
             </p>
         </div>
 
