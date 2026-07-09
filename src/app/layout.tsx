b49fd457c3e623ca75c0f0aafaabba5cda9db7d3
KONSTANTINOS <konstantinos@MacBook-Air-KONSTANTINOS.local>
2026-07-09 20:47:18 +0300

    updata add place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,16 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "./component-custom/bars/navbar";
  
-export const metadata : Metadata = {
+export const metadata: Metadata = {
   title: "Tripaki - Your Personal Trip Planner",
-  description: "Tripaki is a trip planning app that helps you organize your journeys. Create destinations, connect them with transportation, build your itinerary with places to stay and visit, and manage your travel budget in one place.",
-   icons: {
-    icon: "/favicon.png",
+  description: "Tripaki is a trip planning app that helps you organize your journeys.",
+  icons: {
+    icon: [
+      { url: "/favicon.png", type: "image/png", sizes: "48x48" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
-  openGraph: {
-    title: "Tripaki - Your Personal Trip Planner",
-    description: "Tripaki is a trip planning app that helps you organize your journeys. Create destinations, connect them with transportation, build your itinerary with places to stay and visit, and manage your travel budget in one place.",
-     url: "https://tripaki.travel",
-    siteName: "Tripaki",
-    locale: "en_US",
-    type: 'website',
-  },
- 
-}
+};
 
 
   export default async function RootLayout({
